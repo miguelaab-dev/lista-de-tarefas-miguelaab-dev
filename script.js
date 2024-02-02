5bf3f0bde003fb32aef22b6b3794b49d8e052eb3
miguelbraga01 <151753461+miguelaab-dev@users.noreply.github.com>
2024-02-02 00:24:53 -0300

    Resolução de BUG no SPAN de urgência da task
    Linha 49 - Não precisava adicionar este comando visto que criava além do span com a cor correta a palavra do tipo de urgência por cima, resolvido.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const tasks = [
-  {title: "Comprar comida para o gato", type: "Urgente"},
-  {title: "Consertar Computador", type: "Importante"},
-  {title: "Beber água", type: "Normal"},
-  {title: "Enviar relatório trimestral", type: "Importante"},
-  {title: "Fazer exercícios físicos", type: "Normal"},
-  {title: "Agendar consulta médica", type: "Urgente"},
-  {title: "Ler pelo menos um capítulo de um livro", type: "Normal"},
-  {title: "Limpar a despensa", type: "Importante"},
-  {title: "Pagar a conta de energia", type: "Urgente"},
-  {title: "Assistir a um documentário interessante", type: "Normal"},
+  { title: "Comprar comida para o gato", type: "Urgente" },
+  { title: "Consertar Computador", type: "Importante" },
+  { title: "Beber água", type: "Normal" },
+  { title: "Enviar relatório trimestral", type: "Importante" },
+  { title: "Fazer exercícios físicos", type: "Normal" },
+  { title: "Agendar consulta médica", type: "Urgente" },
+  { title: "Ler pelo menos um capítulo de um livro", type: "Normal" },
+  { title: "Limpar a despensa", type: "Importante" },
+  { title: "Pagar a conta de energia", type: "Urgente" },
+  { title: "Assistir a um documentário interessante", type: "Normal" },
 ];
 
 function createTaskItem(task) {
@@ -29,7 +29,7 @@ function createTaskItem(task) {
   );
 
   // CLASSES 
-  
+
   liItem.classList.add("task__item");
   divTaskInfo.classList.add("task-info__container");
   spanTaskType.classList.add("task-type");
@@ -41,11 +41,22 @@ function createTaskItem(task) {
     spanTaskType.classList.add("span-important")
   } else if (task.type === "Normal") {
     spanTaskType.classList.add("span-normal")
-  } 
+  }
 
   // CONTEUDO INTERNO
 
   pTaskTitle.innerText = task.title;
-  spanTaskType.innerHTML = task.type;
-
+ 
+  return liItem
 }
+
+function renderElements(tasksList) {
+  const ulTasks = document.querySelector(".tasks__list")
+
+  tasksList.forEach(task => {
+    const taskItem = createTaskItem(task)
+    ulTasks.appendChild(taskItem)
+  });
+}
+
+renderElements(tasks)
